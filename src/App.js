06fb192e82f59423d4205e5fc7cbaa8f0e51e977
Your Name <you@example.com>
2023-11-25 09:17:28 +0100

    ola

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import Modal from './components/Modal';
 import './index.css';
 
 function App() {
-const name = "nunes"
+  const modevent = () => {
+alert("ola cuzao")
+  }
   return (
     <div className="App">
       <header className="App-header">
-   <div className='bg-rose-50 h-[60px] flex items-center justify-center gap-[10px]'>
-<button className='bg-gray-500 w-[50px] h-[30px] rounded-[8px] border-2'>ola</button><button className='bg-gray-500 w-[50px] h-[50px] rounded-[8px] border-2'>ola</button>
+   <div id='nnn' className='bg-rose-50 h-[60px] flex items-center justify-center gap-[10px]'>
+<button onClick={modevent} className='bg-gray-500 w-[50px] h-[30px] rounded-[8px] border-2'>ola</button>
+<button onClick={() => {}} className='bg-gray-500 w-[50px] h-[50px] rounded-[8px] border-2'>ola</button>
    </div>
       </header>
     </div>
