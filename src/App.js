8a1eeda018c0193dda6122d42235601b9b9ced0b
Your Name <you@example.com>
2023-11-25 06:47:43 +0100

    Eu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Link  from './components/HelloWord';
 import Input from './components/Input';
+import Modal from './components/Modal';
 import './index.css';
 
 function App() {
@@ -9,7 +10,9 @@ const name = "nunes"
   return (
     <div className="App">
       <header className="App-header">
-   
+   <div className='bg-rose-50 h-[60px] flex items-center justify-center gap-[10px]'>
+<button className='bg-gray-500 w-[50px] h-[30px] rounded-[8px] border-2'>ola</button><button className='bg-gray-500 w-[50px] h-[50px] rounded-[8px] border-2'>ola</button>
+   </div>
       </header>
     </div>
   );
---
+++ src/components/Modal.js
@@ -0,0 +1,5 @@
+function Modal(props){
+    return(
+        <div className={props.className}></div>
+    )
+}
