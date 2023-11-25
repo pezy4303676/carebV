8325e3c7183fb4ad3f33177b57f2702cd4e579ef
Your Name <you@example.com>
2023-11-25 05:16:39 +0100

    novo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Link  from './components/HelloWord';
+import './index.css';
+
+function App() {
+const name = "nunes"
+  return (
+    <div className="App">
+      <header className="App-header">
+    <div className="fixed left-7 bg-purple-800 w-10 h-10 text-6xl">loremmm</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HelloWord.js
@@ -0,0 +1,10 @@
+
+function Link (props){
+    return (
+        <div>
+           <a href={props.href}>{props.name}</a>
+        </div>
+    )
+}
+
+export default Link;
