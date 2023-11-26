ee594a9189ff39c7f05ee2114dc11e4768f8fcc5
Your Name <you@example.com>
2023-11-26 09:05:59 +0100

    olaa

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ alert("ola cuzao")
   return (
     <div className="App">
       <header className="App-header">
-   <div className='bg-slate-500 w-[100%] h-[60px] fixed z-10'>olaaaaaaa</div>
-   <div className='bg-slate-900 w-[100%] h-max'>
-    
+   <div className='bg-gradient-to-tl from-gray-700 via-gray-900 to-black w-[100%]  h-[60px] fixed z-10 '></div>
+   <div className='bg-gradient-to-bl from-gray-700 via-gray-900 to-black w-[100%] h-[2000px] '>
+    <br></br><br></br><br></br>
+   <h1 className='text-white'>olaaaaaaa</h1> 
    </div>
       </header>
     </div>
